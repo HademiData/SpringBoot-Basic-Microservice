319eaf5f561449fe08e07cc704415e0c45235ebd
HademiData <adewale.afolabi180221005@st.lasu.edu.ng>
2024-10-09 20:33:37 +0100

    first commit

---
+++ descovery/src/main/java/com/Hademi/descovery/DescoveryApplication.java
@@ -0,0 +1,13 @@
+package com.Hademi.descovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescoveryApplication.class, args);
+	}
+
+}
---
+++ descovery/src/test/java/com/Hademi/descovery/DescoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hademi.descovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DescoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
